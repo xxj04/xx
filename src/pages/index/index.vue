5599d3d8de0f94dab9ca9506807522da10fbc5e3
1451795172@qq.com <1451795172@qq.com>
2023-09-22 14:17:34 +0800

    xx

---
+++ src/pages/index/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+//
+import CustomNavbar from '@/components/CustomNavbar.vue'
+</script>
+
+<template>
+  <CustomNavbar></CustomNavbar>
+  <XtxSwiper />
+  <view> index </view>
+</template>
+
+<style lang="scss"></style>
---
+++ src/pages/my/my.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { useMemberStore } from '@/stores'
+import { http } from '@/utils/http'
+
+const memberStore = useMemberStore()
+
+const getDate = async () => {
+  const res = await http({
+    method: 'GET',
+    url: '',
+    header: {},
+  })
+  console.log(res)
+}
+</script>
+
+<template>
+  <view class="my">
+    <view>会员信息：{{ memberStore.profile }}</view>
+    <button
+      @tap="
+        memberStore.setProfile({
+          nickname: '黑马先锋x',
+          token: '1234',
+        })
+      "
+      size="mini"
+      plain
+      type="primary"
+    >
+      保存用户信息
+    </button>
+
+    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+
+    <button type="warn" size="mini" @tap="getDate()">测试请求</button>
+  </view>
+</template>
+
+<style lang="scss">
+//
+</style>
